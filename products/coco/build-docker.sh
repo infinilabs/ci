d5f1d27be36d257a2e211d16e4b18687564274cd
hardy <luohf@infinilabs.com>
2025-03-07 19:25:41 +0800

    chore: setting for path & logs

--- products/coco/build-docker.sh
+++ products/coco/build-docker.sh
@@ -30,6 +30,9 @@ for t in amd64 arm64; do
   cp -rf $GITHUB_WORKSPACE/$PNAME/bin/$PNAME-linux-$t $WORK/$PNAME-$t
   cp -rf $GITHUB_WORKSPACE/$PNAME/bin/config $WORK/$PNAME-$t
   cp -rf $GITHUB_WORKSPACE/$PNAME/bin/{LICENSE,NOTICE,$PNAME.yml} $WORK/$PNAME-$t
+  # 更新配置
+  echo "path.data: /app/easysearch/data/coco/data" > $WORK/$PNAME-$t/config/$PNAME.yml
+  echo "path.logs: /app/easysearch/data/coco/logs" >> $WORK/$PNAME-$t/config/$PNAME.yml
 
   # ES_DISTRIBUTION_TYPE need change to docker
   sed -i 's/tar/docker/' $WORK/$DNAME-$t/bin/$DNAME-env
