bd8224dc35f214df68bff0c39366bc54fb18e7e4
Hardy <luohoufu@163.com>
2025-03-20 11:27:33 +0800

    fix: docker login

--- certs/init.sh
+++ certs/init.sh
@@ -4,17 +4,18 @@
 # Instruct every Docker daemon to trust that certificate. The way to do this depends on your OS.
 
 CERTS_DIR=/etc/docker/certs.d/registry.infini.dev
-LOCAL_CERT=/usr/local/share/ca-certificates/registry.infini.dev.crt
+LOCAL_DIR=/usr/local/share/ca-certificates
+LOCAL_CERT=$LOCAL_DIR/registry.infini.dev.crt
 DEAMON_JSON=/etc/docker/daemon.json
-mkdir -p $CERTS_DIR
 
 cd $GITHUB_WORKSPACE/certs
+sudo mkdir -p $CERTS_DIR && sudo mkdir -p $LOCAL_DIR && sudo mkdir -p /etc/docker
 
-sudo cp ca.crt $CERTS_DIR/ca.crt
-sudo cp ca.crt $LOCAL_CERT
+sudo cp $GITHUB_WORKSPACE/certs/ca.crt $CERTS_DIR/ca.crt
+sudo cp $GITHUB_WORKSPACE/certs/ca.crt $LOCAL_CERT
 sudo update-ca-certificates
 
 eval "cat <<EOF
 $(cat daemon.json)
-EOF" > $DEAMON_JSON
-cat $DEAMON_JSON
+EOF" > /tmp/daemon.json
+sudo cp /tmp/daemon.json $DEAMON_JSON
