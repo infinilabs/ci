7540ad5f86667f49faa21d453c9999d72ea44a7c
hardy <luohf@infinilabs.com>
2025-05-07 22:04:34 +0800

    chore: for publish signature

--- products/coco-app/generate-latest-json.sh
+++ products/coco-app/generate-latest-json.sh
@@ -11,7 +11,7 @@ echo "PURE_VERSION: $PURE_VERSION"
 get_signature() {
   file="$1"
   oss download -c $GITHUB_WORKSPACE/.oss.yml -k $PRE_UPGRADE_PATH -f $file
-  signature=$(cat $file)
+  signature=$(cat $file | awk 'END {print}')
   echo "$signature"
 }
 
