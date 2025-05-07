6d79a106051dc25d1ccc692ea73dbe7a75d62f6e
hardy <luohf@infinilabs.com>
2025-05-07 17:46:37 +0800

    fix: get signature with curl

--- products/coco-app/generate-latest-json.sh
+++ products/coco-app/generate-latest-json.sh
@@ -3,15 +3,14 @@ set -e
 
 # Check variables
 echo "PRE_UPGRADE_PATH: $PRE_UPGRADE_PATH"
-echo "RELEASE_UPGRADE_PATH: $RELEASE_UPGRADE_PATH"
+echo "UPGRADE_PATH: $UPGRADE_PATH"
 echo "VERSION: $VERSION"
 echo "PURE_VERSION: $PURE_VERSION"
 
 # Function to fetch signatures with error handling
 get_signature() {
-  file="$1"
-  oss download -c $GITHUB_WORKSPACE/.oss.yml -k $PRE_UPGRADE_PATH -f $file
-  signature=$(cat $file)
+  url="$1"
+  signature=$(curl -s "$url")
   if [ $? -ne 0 ]; then
     echo "Error: Failed to download signature from $url"
     return 1
@@ -20,13 +19,13 @@ get_signature() {
 }
 
 # Get the signature for each platform
-signature_darwin_aarch64=$(get_signature "Coco-AI_${VERSION}_arm64.app.tar.gz.sig")
-signature_darwin_x86_64=$(get_signature "Coco-AI_${VERSION}_amd64.app.tar.gz.sig")
-signature_linux_x86_64=$(get_signature "Coco-AI_${VERSION}_amd64.AppImage.sig")
-signature_linux_aarch64=$(get_signature "Coco-AI_${VERSION}_aarch64.AppImage.sig")
-signature_windows_x86_64=$(get_signature "Coco-AI_${VERSION}_x64-setup.exe.sig")
-signature_windows_arm64=$(get_signature "Coco-AI_${VERSION}_arm64-setup.exe.sig")
-signature_windows_i686=$(get_signature "Coco-AI_${VERSION}_x86-setup.exe.sig")
+signature_darwin_aarch64=$(get_signature "${RELEASE_URL}/$PRE_UPGRADE_PATH/Coco-AI_${VERSION}_arm64.app.tar.gz.sig")
+signature_darwin_x86_64=$(get_signature "${RELEASE_URL}/$PRE_UPGRADE_PATH/Coco-AI_${VERSION}_amd64.app.tar.gz.sig")
+signature_linux_x86_64=$(get_signature "${RELEASE_URL}/$PRE_UPGRADE_PATH/Coco-AI_${VERSION}_amd64.AppImage.sig")
+signature_linux_aarch64=$(get_signature "${RELEASE_URL}/$PRE_UPGRADE_PATH/Coco-AI_${VERSION}_aarch64.AppImage.sig")
+signature_windows_x86_64=$(get_signature "${RELEASE_URL}/$PRE_UPGRADE_PATH/Coco-AI_${VERSION}_x64-setup.exe.sig")
+signature_windows_arm64=$(get_signature "${RELEASE_URL}/$PRE_UPGRADE_PATH/Coco-AI_${VERSION}_arm64-setup.exe.sig")
+signature_windows_i686=$(get_signature "${RELEASE_URL}/$PRE_UPGRADE_PATH/Coco-AI_${VERSION}_x86-setup.exe.sig")
 
 # Create the base JSON structure
 cat > .latest.json <<EOF
@@ -37,31 +36,31 @@ cat > .latest.json <<EOF
   "platforms": {
     "darwin-aarch64": {
       "signature": "${signature_darwin_aarch64}",
-      "url": "${RELEASE_URL}/${RELEASE_UPGRADE_PATH}/Coco-AI_${VERSION}_arm64.app.tar.gz"
+      "url": "${RELEASE_URL}/${UPGRADE_PATH}/Coco-AI_${VERSION}_arm64.app.tar.gz"
     },
     "darwin-x86_64": {
       "signature": "${signature_darwin_x86_64}",
-      "url": "${RELEASE_URL}/${RELEASE_UPGRADE_PATH}/Coco-AI_${VERSION}_amd64.app.tar.gz"
+      "url": "${RELEASE_URL}/${UPGRADE_PATH}/Coco-AI_${VERSION}_amd64.app.tar.gz"
     },
     "linux-x86_64": {
       "signature": "${signature_linux_x86_64}",
-      "url": "${RELEASE_URL}/${RELEASE_UPGRADE_PATH}/Coco-AI_${VERSION}_amd64.AppImage"
+      "url": "${RELEASE_URL}/${UPGRADE_PATH}/Coco-AI_${VERSION}_amd64.AppImage"
     },
     "linux-aarch64": {
       "signature": "${signature_linux_aarch64}",
-      "url": "${RELEASE_URL}/${RELEASE_UPGRADE_PATH}/Coco-AI_${VERSION}_aarch64.AppImage"
+      "url": "${RELEASE_URL}/${UPGRADE_PATH}/Coco-AI_${VERSION}_aarch64.AppImage"
     },
     "windows-x86_64": {
       "signature": "${signature_windows_x64}",
-      "url": "${RELEASE_URL}/${RELEASE_UPGRADE_PATH}/Coco-AI_${VERSION}_x64-setup.exe"
+      "url": "${RELEASE_URL}/${UPGRADE_PATH}/Coco-AI_${VERSION}_x64-setup.exe"
     },
     "windows-aarch64": {
       "signature": "${signature_windows_arm64}",
-      "url": "${RELEASE_URL}/${RELEASE_UPGRADE_PATH}/Coco-AI_${VERSION}_arm64-setup.exe"
+      "url": "${RELEASE_URL}/${UPGRADE_PATH}/Coco-AI_${VERSION}_arm64-setup.exe"
     },
     "windows-i686": {
       "signature": "${signature_windows_i686}",
-      "url": "${RELEASE_URL}/${RELEASE_UPGRADE_PATH}/Coco-AI_${VERSION}_x86-setup.exe"
+      "url": "${RELEASE_URL}/${UPGRADE_PATH}/Coco-AI_${VERSION}_x86-setup.exe"
     }
   }
 }
