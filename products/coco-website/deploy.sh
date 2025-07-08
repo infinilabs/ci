5d6e3457242c11f7b7def28f4075b127a8500754
hardy <luohf@infinilabs.com>
2025-07-08 18:54:45 +0800

    fix: deploy overwrite file

--- products/coco-website/deploy.sh
+++ products/coco-website/deploy.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
-ls -lh /opt/coco-website.zip
-unzip -q /opt/coco-website.zip -d /opt/coco-website
-chown -R www-data:www-data /opt/coco-website
+PNAME="coco-website"
+WORK="/opt/$PNAME"
+EXT="zip"
+
+if [[ ! -d "$WORK" ]]; then
+  echo "Creating directory $WORK"
+  mkdir -p "$WORK" || {
+    echo "Failed to create directory $WORK"
+    exit 1
+  }
+fi
+
+ls -lh "$WORK.$EXT" || true
+unzip -qo "$WORK.$EXT" -d "$WORK" || {
+  echo "Failed to unzip $WORK.$EXT"
+  exit 1
+}
+
+chown -R www-data:www-data "$WORK" || {
+  echo "Failed to change ownership of $WORK"
+  exit 1
+}
+
+rm -rvf "$WORK.$EXT" || {
+  echo "Failed to remove $WORK.$EXT"
+  exit 1
+}
