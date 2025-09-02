d6640a569ec2f0fded7745853a58f2502fc7aad1
hardy <luohf@infinilabs.com>
2025-09-02 10:52:57 +0800

    fix: clean staging cache

--- products/easysearch/build-snapshot.sh
+++ products/easysearch/build-snapshot.sh
@@ -92,7 +92,7 @@ echo
 if [[ "$(echo "$ONLY_DOCKER" | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
   echo "Publish Docker <Only> image no need to flush cache"
 else
-  curl -H "X-Token: $TOKEN" "$RELEASE_URL/_flush?versions=$VERSION" > /dev/null
+  ssh staging "curl -H 'X-Token: $TOKEN' '$RELEASE_URL/_flush?versions=$VERSION-SNAPSHOT' > /dev/null"
   echo "Flushed plugin cache and page cache ..."
 fi
 echo
