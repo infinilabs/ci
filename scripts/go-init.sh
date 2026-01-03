8c9aaf27e209fc4b6671e41c502153b885fdf69a
hardy <luohf@infinilabs.com>
2026-01-03 11:17:24 +0800

    chore: update actions version

--- scripts/go-init.sh
+++ scripts/go-init.sh
@@ -9,9 +9,3 @@ ln -s $GITHUB_WORKSPACE $WORK
 echo "Build path is $WORK"
 # update Makefile
 cp -rf $WORK/products/framework/Makefile $WORK/framework
-
-# --- Configure go environment ---
-echo "🔧 Configuring Go environment..."
-export GOEXPERIMENT=greenteagc
-echo "export GOEXPERIMENT=greenteagc" >> $HOME/.bashrc
-source $HOME/.bashrc
