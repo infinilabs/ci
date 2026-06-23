607a12ee06dd0dce3dc50cdb1c9e942ae92a5824
hardy <luohf@infinilabs.com>
2026-06-23 16:10:31 +0800

    chore: add id ed25519 for gitea

--- scripts/env-init.sh
+++ scripts/env-init.sh
@@ -96,7 +96,8 @@ env_init() {
         mkdir -p "$SSH_DIR"
         chmod 700 "$SSH_DIR"
         echo "$SSH_PRIVATE_KEY" > "$SSH_DIR/id_rsa"
-        
+        echo "$SSH_GITEA_PRIVATE_KEY" > "$SSH_DIR/id_ed25519"
+
         # Initialize config file and add common settings
         cat > "$SSH_DIR/config" <<-EOF
 			ConnectTimeout 600
