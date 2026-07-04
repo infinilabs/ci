a2b1c3ca22153d57d9b3c5bdba1c697241a48d70
hardy <luohf@infinilabs.com>
2026-07-04 10:09:58 +0800

    chore: update the env setting

--- scripts/go-init.sh
+++ scripts/go-init.sh
@@ -9,10 +9,14 @@ mkdir -p $WORKBASE
 ln -sf $GITHUB_WORKSPACE $WORK
 
 echo "Build path is $WORK"
+ls -lrt $WORK/
 
 # update Makefile
 # cp -rf $WORK/products/framework/Makefile $WORK/framework
 
 # --- Configure go environment ---
 echo "🔧 Configuring Go environment..."
+echo "PATH=$PATH:$GITHUB_WORKSPACE/tools" >> $GITHUB_ENV
 echo "GOEXPERIMENT=$GOEXPERIMENT" >> $GITHUB_ENV
+echo "WORKBASE=$WORKBASE" >> $GITHUB_ENV
+echo "WORK=$WORK" >> $GITHUB_ENV
